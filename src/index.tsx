2eca9d893636f0dcb39e352af1aace20426af564
Jihun Song <songjihun940409@gmail.com>
2023-02-20 18:37:53 +0900

    1.2 AUTHENTICATION - React Hook Form part Two

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,9 @@ import ReactDOM from "react-dom/client";
 import { ApolloProvider } from "@apollo/client";
 import { RouterProvider } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
-import "./styles/styles.css";
+import "./styles/global.css";
 import { client } from "./apollo";
 import { router } from "./router";
-
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
--- src/routes/logged-out-router.tsx
+++ src/routes/logged-out-router.tsx
@@ -1,22 +1,31 @@
 import { useForm } from "react-hook-form";
 
+interface IForm {
+  email: string;
+  password: string;
+}
+
 export default function LoggedOutRouter() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm<IForm>();
   const onSubmit = (data: any) => console.log(data);
   const onInValid = () => {
-    console.log(errors);
+    console.log("can't create account");
   };
+  console.log(errors.email?.type);
   return (
     <div>
       <h1>Logged Out</h1>
       <form onSubmit={handleSubmit(onSubmit, onInValid)}>
         <div>
-          <input {...register("email", { required: "This is required", pattern: /^[A-Za-z]+@gmail.com$/ })} placeholder="email" />
+          <input {...register("email", { required: "This is required", pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/ })} placeholder="email" />
+          {errors.email?.message && <span className="font-bold text-red-600">{errors.email?.message}</span>}
+          {errors.email?.type === "pattern" && <span className="font-bold text-red-600">Only Gmail allowed</span>}
         </div>
+
         <div>
           <input {...register("password", { required: true })} type="password" placeholder="password" />
         </div>

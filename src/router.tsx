ebf1da84bf557af2f91825c4f7f98a935a2cabfb
Jihun Song <songjihun940409@gmail.com>
2023-02-22 18:12:37 +0900

    1.5 AUTHENTICATION - Form Login

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Login } from "./routes/login";
+import Login from "./routes/login";
 import Root from "./components/Root";
 import { CreateAccount } from "./routes/create-account";
 
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,20 +1,31 @@
-export const Login = () => {
+import { useForm } from "react-hook-form";
+
+interface ILoginForm {
+  email: string;
+  password: string;
+}
+
+export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ILoginForm>();
+  const onSubmit = (data: any) => {};
+  console.log(errors);
   return (
     <div className="flex h-screen items-center justify-center bg-gray-800">
-      <div className="w-full max-w-lg rounded-lg bg-white py-10 text-center">
+      <div className="w-full max-w-lg rounded-lg bg-white pt-10 pb-7 text-center">
         <h3 className="text-2xl text-gray-800">Log In</h3>
-        <form className="mt-5 flex flex-col px-5">
-          <input
-            placeholder="Email"
-            className="mb-3 rounded-lg border-2 border-green-600 border-opacity-50 bg-gray-100 px-3 py-2 shadow-inner focus:outline-none"
-          />
-          <input
-            placeholder="Password"
-            className="rounded-lg border-2 border-green-600 border-opacity-50 bg-gray-100 px-3 py-2 shadow-inner focus:outline-none"
-          />
-          <button className="mt-3 rounded-lg bg-gray-800 py-3 px-5 text-lg text-white focus:outline-none hover:opacity-90">Log In</button>
+        <form onSubmit={handleSubmit(onSubmit)} className="mt-5 grid gap-5 px-5">
+          <input {...register("email", { required: "Email is required" })} type="email" placeholder="Email" className="input mb-3" />
+          {errors.email?.message && <span className="font-medium text-red-500">{errors.email?.message}</span>}
+          <input {...register("password", { required: "Password is required", minLength: 10 })} type="password" placeholder="Password" className="input" />
+          {errors.password?.message && <span className="font-medium text-red-500">{errors.password?.message}</span>}
+          {errors.password?.type === "minLength" && <span className="font-medium text-red-500">Password must be more than 10 chars.</span>}
+          <button className="btn mt-3">Log In</button>
         </form>
       </div>
     </div>
   );
-};
+}

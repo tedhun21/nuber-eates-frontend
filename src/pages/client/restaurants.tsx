abf56d3eded351e889f5fca368478333f1d5663d
Jihun Song <songjihun940409@gmail.com>
2023-03-06 22:32:22 +0900

    3.1 RESTAURANTS - Categories Style

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -42,5 +42,24 @@ export const Restaurants = () => {
       },
     },
   });
-  return <h1>Restaurants</h1>;
+  console.log(data);
+  return (
+    <div>
+      <form className="flex w-full items-center justify-center bg-gray-800 py-40">
+        <input className="input w-3/12 rounded-md border-0" type="Search" placeholder="Search Restaurants" />
+      </form>
+      {!loading && (
+        <div className="mx-auto mt-8 max-w-screen-2xl">
+          <div className="mx-auto flex max-w-screen-sm justify-around">
+            {data?.allCategories.categories?.map((category) => (
+              <div className="flex cursor-pointer flex-col items-center">
+                <div className="h-14 w-14 rounded-full bg-cover hover:bg-gray-200" style={{ backgroundImage: `url(${category.coverImg})` }}></div>
+                <span className="mt-1 text-center text-sm font-medium">{category.name}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };

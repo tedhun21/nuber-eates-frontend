826b1e2ffcaa677da2abbb4dd56bfa39eedf0749
Jihun Song <songjihun940409@gmail.com>
2023-03-09 21:42:51 +0900

    3.6 RESTAURANTS - Category Page

--- src/gql/gql.ts
+++ src/gql/gql.ts
@@ -16,6 +16,7 @@ const documents = {
     "\n  fragment RestaurantParts on Restaurant {\n    id\n    name\n    coverImg\n    category {\n      name\n    }\n    address\n    isPromoted\n  }\n": types.RestaurantPartsFragmentDoc,
     "\n  fragment CategoryParts on Category {\n    id\n    name\n    coverImg\n    slug\n    restaurantCount\n  }\n": types.CategoryPartsFragmentDoc,
     "\n  query Me {\n    me {\n      id\n      email\n      role\n      verified\n    }\n  }\n": types.MeDocument,
+    "\n  query AllCategories {\n    allCategories {\n      ok\n      error\n      categories {\n        ...CategoryParts\n      }\n    }\n  }\n": types.AllCategoriesDocument,
     "\n  query Category($categoryInput: CategoryInput!) {\n    category(input: $categoryInput) {\n      ok\n      error\n      totalPages\n      totalResults\n      restaurants {\n        ...RestaurantParts\n      }\n    }\n  }\n": types.CategoryDocument,
     "\n  query RestaurantsPage($restaurantsInput: RestaurantsInput!) {\n    allCategories {\n      ok\n      error\n      categories {\n        ...CategoryParts\n      }\n    }\n    restaurants(input: $restaurantsInput) {\n      ok\n      error\n      totalPages\n      results {\n        ...RestaurantParts\n      }\n    }\n  }\n": types.RestaurantsPageDocument,
     "\n  query SearchRestaurant($SearchRestaurantInput: SearchRestaurantInput!) {\n    searchRestaurant(input: $SearchRestaurantInput) {\n      ok\n      error\n      totalPages\n      totalResults\n      restaurants {\n        ...RestaurantParts\n      }\n    }\n  }\n": types.SearchRestaurantDocument,
@@ -53,6 +54,10 @@ export function graphql(source: "\n  fragment CategoryParts on Category {\n    i
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
 export function graphql(source: "\n  query Me {\n    me {\n      id\n      email\n      role\n      verified\n    }\n  }\n"): (typeof documents)["\n  query Me {\n    me {\n      id\n      email\n      role\n      verified\n    }\n  }\n"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n  query AllCategories {\n    allCategories {\n      ok\n      error\n      categories {\n        ...CategoryParts\n      }\n    }\n  }\n"): (typeof documents)["\n  query AllCategories {\n    allCategories {\n      ok\n      error\n      categories {\n        ...CategoryParts\n      }\n    }\n  }\n"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
--- src/pages/client/category.tsx
+++ src/pages/client/category.tsx
@@ -1,8 +1,21 @@
 import { useQuery } from "@apollo/client";
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Restaurant } from "../../components/restaurant";
 import { graphql } from "../../gql";
-import { CategoryQuery, CategoryQueryVariables } from "../../gql/graphql";
+import { AllCategoriesQuery, AllCategoriesQueryVariables, CategoryQuery, CategoryQueryVariables } from "../../gql/graphql";
+
+const CATEGORIES_QUERY = graphql(`
+  query AllCategories {
+    allCategories {
+      ok
+      error
+      categories {
+        ...CategoryParts
+      }
+    }
+  }
+`);
 
 const CATEGORY_QUERY = graphql(`
   query Category($categoryInput: CategoryInput!) {
@@ -23,16 +36,60 @@ interface ICategoryParams {
 }
 
 export const Category = () => {
+  const [page, setPage] = useState(1);
   const { slug } = useParams<ICategoryParams>();
   useEffect(() => {});
-
-  const { data, loading, error } = useQuery<CategoryQuery, CategoryQueryVariables>(CATEGORY_QUERY, {
+  const { data: categoriesData } = useQuery<AllCategoriesQuery, AllCategoriesQueryVariables>(CATEGORIES_QUERY);
+  const { data: categoryData } = useQuery<CategoryQuery, CategoryQueryVariables>(CATEGORY_QUERY, {
     variables: {
       categoryInput: {
         slug,
       },
     },
   });
-  console.log(data);
-  return <h1>category</h1>;
+  const onPrevClick = () => {
+    setPage((current) => current - 1);
+  };
+  const onNextClick = () => {
+    setPage((current) => current + 1);
+  };
+  console.log(categoryData);
+  return (
+    <div>
+      <div className="mx-auto flex max-w-screen-sm justify-around">
+        {categoriesData?.allCategories.categories?.map((category) => (
+          <Link to={`/category/${category.slug}`}>
+            <div className="flex flex-col items-center">
+              <div className="h-16 w-16 rounded-full bg-cover" style={{ backgroundImage: `url(${category.coverImg})` }}></div>
+              <span>{category.name}</span>
+            </div>
+          </Link>
+        ))}
+      </div>
+      <div className="mt-16 grid gap-7 gap-x-5 gap-y-10 md:grid-cols-3">
+        {categoryData?.category.restaurants?.map((restaurant) => (
+          <Restaurant key={restaurant.id} id={restaurant.id} coverImg={restaurant.coverImg} name={restaurant.name} categoryName={restaurant.category?.name} />
+        ))}
+      </div>
+      <div className="mx-auto mt-10 grid max-w-md grid-cols-3 items-center justify-center text-center pb-20">
+        {page > 1 ? (
+          <button onClick={onPrevClick} className="text-2xl font-medium">
+            &larr;
+          </button>
+        ) : (
+          <div></div>
+        )}
+        <span>
+          Page {page} of {categoryData?.category.totalPages}
+        </span>
+        {page !== categoryData?.category.totalPages ? (
+          <button onClick={onNextClick} className="text-2xl font-medium">
+            &rarr;
+          </button>
+        ) : (
+          <div></div>
+        )}
+      </div>
+    </div>
+  );
 };

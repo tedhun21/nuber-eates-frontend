396c5bd61672a8e8ad4a0ae5f79b3f946143375c
Jihun Song <songjihun940409@gmail.com>
2023-03-14 18:47:43 +0900

    4.2 OWNER DASHBOARD - Create Restaurant part Two

--- src/pages/owner/add-restaurants.tsx
+++ src/pages/owner/add-restaurants.tsx
@@ -1,7 +1,9 @@
 import { useMutation } from "@apollo/client";
+import { useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import Button from "../../components/button";
+import FormError from "../../components/form-error";
 import { graphql } from "../../gql";
 import { CreateRestaurantMutation, CreateRestaurantMutationVariables } from "../../gql/graphql";
 
@@ -19,31 +21,67 @@ interface IFormProp {
   name: string;
   categoryName: string;
   address: string;
-  coverImg: string;
+  file: FileList;
 }
 
 export const AddRestaurant = () => {
-  const [createRestaurantMutation, { loading, data }] = useMutation<CreateRestaurantMutation, CreateRestaurantMutationVariables>(CREATE_RESTAURANT_MUTAION);
+  const onCompleted = (data: CreateRestaurantMutation) => {
+    const {
+      createRestaurant: { ok, error, restaurantId },
+    } = data;
+    if (ok) {
+      setUploading(false);
+    }
+  };
+  const [createRestaurantMutation, { data }] = useMutation<CreateRestaurantMutation, CreateRestaurantMutationVariables>(CREATE_RESTAURANT_MUTAION, {
+    onCompleted,
+  });
   const {
     register,
     handleSubmit,
     formState: { errors, isValid },
   } = useForm<IFormProp>();
-  const onSubmit = (data: IFormProp) => {
-    console.log(data);
+  const [uploading, setUploading] = useState(false);
+  const onSubmit = async ({ name, categoryName, address, file }: IFormProp) => {
+    try {
+      setUploading(true);
+      const actualFile = file[0];
+      const formBody = new FormData();
+      formBody.append("file", actualFile);
+      const { url: coverImg } = await (
+        await fetch("http://localhost:4000/uploads/", {
+          method: "POST",
+          body: formBody,
+        })
+      ).json();
+      createRestaurantMutation({
+        variables: {
+          createRestaurantInput: {
+            name,
+            categoryName,
+            address,
+            coverImg,
+          },
+        },
+      });
+      setUploading(false);
+    } catch (e) {
+      console.log(e);
+    }
   };
   return (
-    <div className="container">
+    <div className="container mt-52 flex flex-col items-center">
       <Helmet>
         <title>Add Restaurant | Nuber Eats</title>
       </Helmet>
-      <h1>add restaurants</h1>
-      <form className="jusrify-center flex flex-col items-center" onSubmit={handleSubmit(onSubmit)}>
+      <h4 className="mb-3 text-2xl font-semibold">add restaurants</h4>
+      <form className="mt-5 mb-5 grid w-full max-w-screen-sm gap-3" onSubmit={handleSubmit(onSubmit)}>
         <input {...register("name", { required: "Name is required." })} type="text" placeholder="Name" />
         <input {...register("categoryName", { required: "Category Name is required." })} type="text" placeholder="Category Name" />
         <input {...register("address", { required: "Address is required." })} type="text" placeholder="Address" />
-        <input {...register("coverImg", { required: "Cover Image is required." })} type="text" placeholder="Cover Image" />
-        <Button loading={loading} canClick={isValid} actionText="Create Restaurant" />
+        <input {...register("file", { required: "Cover Image is required." })} type="file" placeholder="Cover Image" accept="image/*" />
+        <Button loading={uploading} canClick={isValid} actionText="Create Restaurant" />
+        {data?.createRestaurant.error && <FormError errorMessage={data.createRestaurant.error} />}
       </form>
     </div>
   );
